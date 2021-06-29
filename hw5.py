8fe5d907c4539a256d4f510dcea9340f682b29c9
BESTTEST4YOU <81219764+BESTTEST4YOU@users.noreply.github.com>
2021-06-29 20:21:44 +0300

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -119,27 +119,25 @@ import json
 profit = {}
 pr = {}
 prof = 0
-prof_aver = 0
+aver_profit = 0
 i = 0
 with open('5.7.txt', 'r') as file:
     for line in file:
-        name, firm, earning, damage = line.split()
-        profit[name] = int(earning) - int(damage)
+        name, firm, earning, loss = line.split()
+        profit[name] = int(earning) - int(loss)
         if profit.setdefault(name) >= 0:
             prof = prof + profit.setdefault(name)
             i += 1
     if i != 0:
-        prof_aver = prof / i
-        print(f'Прибыль средняя - {prof_aver:.2f}')
+        aver_profit = prof / i
+        print(f'Average profit:\n{aver_profit:}')
     else:
-        print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
-    pr = {'средняя прибыль': round(prof_aver)}
+        print('No profit')
+    pr = {'average profit': round(aver_profit)}
     profit.update(pr)
-    print(f'Прибыль каждой компании - {profit}')
+    print(f'Companies profit:\n{profit}')
 
 with open('5.7.json', 'w') as write_js:
     json.dump(profit, write_js)
-
     js_str = json.dumps(profit)
-    print(f'Создан файл с расширением json со следующим содержимым: \n '
-          f' {js_str}')
+    print(f'json: \n{js_str}')
