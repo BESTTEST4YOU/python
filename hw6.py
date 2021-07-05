16faf3d0a19d139c3cc365a67193d3d60c4fd1ad
BESTTEST4YOU <81219764+BESTTEST4YOU@users.noreply.github.com>
2021-07-05 15:57:27 +0300

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -162,3 +162,41 @@ PoliceCar = PoliceCar(80, 'black', 'Ford', True)
 print('\nPoliceCar:\n' + PoliceCar.go(), PoliceCar.rspeed(), PoliceCar.turn('right'))
 
 
+# 5. Реализовать класс Stationery (канцелярская принадлежность).
+# Определить в нем атрибут title (название) и метод draw (отрисовка).
+# Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка),
+# Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение
+# метода draw.
+# Для каждого из классов методы должен выводить уникальное сообщение.
+# Создать экземпляры классов и проверить,
+# что выведет описанный метод для каждого экземпляра.
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'"Запуск отрисовки"'
+
+
+class Pen(Stationery):
+    def draw(self):
+        return f'"Запуск отрисовки":  {self.title}'
+
+
+class Pencil(Stationery):
+    def draw(self):
+        return f'"Запуск отрисовки":  {self.title}'
+
+
+class Handle(Stationery):
+    def draw(self):
+        return f'"Запуск отрисовки":  {self.title}'
+
+
+pen = Pen('Ручкой')
+pencil = Pencil('Карандашом')
+handle = Handle('Маркером')
+print(pen.draw() + '\n' + pencil.draw() + '\n' + handle.draw())
+
+
