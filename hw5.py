b0c643a4b209640c8f5d142bb03a4be9b81f4efb
BESTTEST4YOU <81219764+BESTTEST4YOU@users.noreply.github.com>
2021-07-09 16:53:41 +0300

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -16,6 +16,13 @@ content = f.readlines()
 print(content)
 f.close()
 
+#короткий варинат кода:
+with open('5.1.txt', 'w', encoding='utf-8') as f:
+    line = ' '
+    while line != '':
+        line = input()
+        print(line, file=f)
+
 # 2. Создать текстовый файл (не программно), сохранить в нем
 # несколько строк, выполнить подсчет количества строк,
 # количества слов в каждой строке.
@@ -28,6 +35,13 @@ content = words.read()
 content = content.split()
 print(f'words: {len(content)}')
 words.close()
+
+#короткий варинат кода:
+with open('5.2.txt', 'r', encoding='utf-8') as f:
+    text = f.readlines()
+    for i, line in enumerate(text, 1):
+        words = len(line.split())
+        print(f'{i}ая строка содержит {words} слов')
 
 # 3. Создать текстовый файл (не программно), построчно записать фамилии
 # сотрудников и величину их окладов. Определить, кто из сотрудников
@@ -45,6 +59,17 @@ with open('5.3.txt', 'r') as salary:
         sal.append(i[1])
         avsalary = round(sum(map(int, sal)) / len(sal), 1)
 print(f'Salary > 20.000 {avsal}\nAverage salary = {avsalary}')
+
+# 2 вариант исполнения кода:
+total_w = 0
+empl = 0
+with open('5.3.txt', 'r') as salary:
+    for line in salary:
+        total_w += float(line.split()[1])
+        empl += 1
+        if float(line.split()[1]) > 20000:
+            print(line.split()[0])
+print('Средняя величина дохода: ', round(total_w / empl, 2))
 
 # 4. Создать (не программно) текстовый файл со следующим содержимым:
 # One — 1
@@ -65,6 +90,14 @@ with open('5.4.txt', 'r', encoding='utf-8') as first:
 with open('5.4.1.txt', 'w') as file_obj_2:
     file_obj_2.writelines(second)
 
+# еще один вариант кода:
+res = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+with open('5.4.1.txt', 'w', encoding='utf-8') as f_write:
+    with open('5.4.txt', 'r', encoding='utf-8') as f_read:
+        for line in f_read:
+            f_write.writelines([line.replace(line.split()[0], res[line.split()[0]])])
+
+
 # 5. Создать (программно) текстовый файл, записать в него программно набор чисел,
 # разделенных пробелами. Программа должна подсчитывать сумму чисел в файле
 # и выводить ее на экран.
@@ -81,6 +114,15 @@ def my_func():
             print('Error')
 my_func()
 
+# еще один вариант кода:
+from random import randint
+
+with open('5.5.txt', 'w', encoding='utf-8') as file_obj:
+    li = [randint(1, 20) for _ in range(50)]
+    file_obj.write(' '.join(map(str, li)))
+
+print('Сумма чисел: ', sum(li))
+
 # 6. Необходимо создать (не программно) текстовый файл, где каждая строка
 # описывает учебный предмет и наличие лекционных, практических и
 # лабораторных занятий по этому предмету и их количество. Важно,
@@ -95,6 +137,14 @@ my_func()
 # Пример словаря:
 # {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 
+with open('5.6.txt', 'r', encoding='utf-8') as file:
+    text = file.readlines()
+    for line in text:
+        new = ''
+        for el in line:
+            new = ''.join([new, (el if el in '0123456789' else ' ')])
+        res = [int(i) for i in new.split()]
+        print(f'{line.split()[0]} - {sum(res)} часов')
 
 
 # 7. Создать (не программно) текстовый файл, в котором каждая строка должна содержать данные о фирме: название, форма собственности, выручка, издержки.
@@ -119,25 +169,37 @@ import json
 profit = {}
 pr = {}
 prof = 0
-aver_profit = 0
+prof_aver = 0
 i = 0
 with open('5.7.txt', 'r') as file:
     for line in file:
-        name, firm, earning, loss = line.split()
-        profit[name] = int(earning) - int(loss)
+        name, firm, earning, damage = line.split()
+        profit[name] = int(earning) - int(damage)
         if profit.setdefault(name) >= 0:
             prof = prof + profit.setdefault(name)
             i += 1
     if i != 0:
-        aver_profit = prof / i
-        print(f'Average profit:\n{aver_profit:}')
+        prof_aver = prof / i
+        print(f'Прибыль средняя - {prof_aver:.2f}')
     else:
-        print('No profit')
-    pr = {'average profit': round(aver_profit)}
+        print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
+    pr = {'средняя прибыль': round(prof_aver)}
     profit.update(pr)
-    print(f'Companies profit:\n{profit}')
+    print(f'Прибыль каждой компании - {profit}')
 
 with open('5.7.json', 'w') as write_js:
     json.dump(profit, write_js)
+
     js_str = json.dumps(profit)
-    print(f'json: \n{js_str}')
+    print(f'Создан файл с расширением json со следующим содержимым: \n '
+          f' {js_str}')
+
+#короткий варинат кода:
+import json
+
+with open('5.7.json', 'w', encoding='utf-8') as f_write:
+    with open('5.7.txt', 'r', encoding='utf-8') as f_read:
+        profit = {line.split()[0]: int(line.split()[2]) - int(line.split()[3]) for line in f_read}
+        res = [profit,{'average_profit': sum([int(i) for i in profit.values() if int(i) > 0]) /
+                       len([int(i) for i in profit.values() if int(i) > 0])}]
+    json.dump(res, f_write)
